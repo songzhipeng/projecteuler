ba8b8269e0e70dd9b8dc1e65abc058a46e708b2c
机智的大肥羊 <songzhipeng@me.com>
2016-01-04 14:26:22 +0800

    default

--- 0.py
+++ 0.py
@@ -1,4 +1,16 @@
 #coding:utf-8
 
-#0
+#38
 
+
+a = '2413'
+a.sort()
+print a
+
+# for x in xrange(1,10000):
+# 	s = ''
+# 	for y in xrange(1,10):
+# 		s += str(x*y)
+# 		if len(s)>=9:
+# 			break
+# 	if len(s)==9:
