dcc8f4ead520ee08322f20a821b0f9dae8af7309
机智的大肥羊 <songzhipeng@me.com>
2016-01-04 14:29:57 +0800

    default

--- 0.py
+++ 0.py
@@ -2,15 +2,15 @@
 
 #38
 
-
-a = '2413'
-a.sort()
-print a
-
-# for x in xrange(1,10000):
-# 	s = ''
-# 	for y in xrange(1,10):
-# 		s += str(x*y)
-# 		if len(s)>=9:
-# 			break
-# 	if len(s)==9:
+res = 0
+for x in xrange(1,10000):
+	s = ''
+	for y in xrange(1,10):
+		s += str(x*y)
+		if len(s)>=9:
+			break
+	if sorted(s) == ['1','2','3','4','5','6','7','8','9']:
+		print s
+		res = max(res, int(s))
+print '>'
+print res
